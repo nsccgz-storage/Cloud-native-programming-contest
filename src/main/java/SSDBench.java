45a54cb2be78558269c1b1fa9d00e5fc31abd573
WYF <w.y.ff@163.com>
2021-09-27 15:27:58 +0800

    test bench

--- src/main/java/SSDBench.java
+++ src/main/java/SSDBench.java
@@ -1,4 +1,4 @@
-import java.nio.channels.FileChannel   ;
+import java.nio.channels.FileChannel;
 import java.nio.ByteBuffer;
 import java.io.RandomAccessFile;
 import java.io.File;
@@ -7,29 +7,54 @@ import java.io.IOException;
 public class SSDBench {
 	public static void main(String []args) {
 		try {
-			int ret;
-			System.out.println("Hello World");
 			String dbPath = "/Users/wyf/0code/nsccgz-storage/Cloud-native-programming-contest/testDB/db.data";
 			FileChannel fileChannel = new RandomAccessFile(new File(dbPath), "rw").getChannel();
-			byte[] data = new byte[4096];
-			long position = 1024L;
-			// 指定 position 写入 4kb 的数据
-			fileChannel.write(ByteBuffer.wrap(data), position);
-			// 从当前文件指针的位置写入 4kb 的数据
-			fileChannel.write(ByteBuffer.wrap(data));
+			// byte[] data = new byte[4096];
+			// long position = 1024L;
+			// // 指定 position 写入 4kb 的数据
+			// fileChannel.write(ByteBuffer.wrap(data), position);
+			// // 从当前文件指针的位置写入 4kb 的数据
+			// fileChannel.write(ByteBuffer.wrap(data));
 
-			// 读
-			ByteBuffer buffer = ByteBuffer.allocate(4096);
-			position = 1024L;
-			// 指定 position 读取 4kb 的数据
-			ret = fileChannel.read(buffer,position);
-			System.out.println(ret);
-			// 从当前文件指针的位置读取 4kb 的数据
-			ret = fileChannel.read(buffer);
-			System.out.println(ret);
+			// // 读
+			// ByteBuffer buffer = ByteBuffer.allocate(4096);
+			// position = 1024L;
+			// // 指定 position 读取 4kb 的数据
+			// ret = fileChannel.read(buffer,position);
+			// System.out.println(ret);
+			// // 从当前文件指针的位置读取 4kb 的数据
+			// ret = fileChannel.read(buffer);
+			// System.out.println(ret);
+			// benchFileChannel(fileChannel, 1, 4096);
+			benchFileChannel(fileChannel, 1, 64*1024);
 		} catch(IOException ie) {
 			ie.printStackTrace();
 		}  
 	}
+
+	public static void benchFileChannel(FileChannel fileChannel, int thread, int ioSize) throws IOException {
+		// thread = 1
+		// long totalBenchSize = 4L*1024L*1024L*1024L; // 4GiB
+		long totalBenchSize = 1L*1024L*1024L*1024L; // 1iB
+		assert(totalBenchSize % ioSize == 0);
+		long totalBenchCount = totalBenchSize/ioSize;
+		byte[] data = new byte[ioSize];
+		long curPosition = 0L;
+		long maxPosition = totalBenchSize;
+		long startTime = System.nanoTime();    
+		for (int i = 0; i < maxPosition; i++){
+			fileChannel.write(ByteBuffer.wrap(data), curPosition);
+			fileChannel.force(true);
+			curPosition += ioSize;
+		}
+		long elapsedTime = System.nanoTime() - startTime;
+		double elapsedTimeS = (double)elapsedTime/(1000*1000*1000);
+		double totalBenchSizeMiB = totalBenchSize/(1024*1024);
+		System.out.println(elapsedTime);
+		System.out.println(totalBenchSize);
+		System.out.println("Bandwidth : " + (totalBenchSizeMiB)/(elapsedTimeS) + " MiB/s");
+		System.out.println("IOPS : " + totalBenchCount/elapsedTimeS + " op/s");
+	}
+
 	
 }
