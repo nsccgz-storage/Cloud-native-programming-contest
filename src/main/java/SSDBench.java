f691d4c5b935b112d3968db30150fcbb5e61922d
WYF <w.y.ff@163.com>
2021-09-27 16:45:52 +0800

    test concurrent write

--- src/main/java/SSDBench.java
+++ src/main/java/SSDBench.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class SSDBench {
 	public static void main(String []args) {
@@ -21,9 +22,12 @@ public class SSDBench {
 
 			System.out.println("type,thread,ioSize,bandwidth,iops");
 			int[] ioSizes = {4*1024, 8*1024, 16*1024, 32*1024, 64*1024, 128*1024, 256*1024, 512*1024,1024*1024};
-			for (int i = 0; i < ioSizes.length; i++){
-				benchFileChannelWrite(fileChannel, totalBenchSize, ioSizes[i]); // ioSize = 64KiB
-			}
+			// for (int i = 0; i < ioSizes.length; i++){
+			// 	benchFileChannelWrite(fileChannel, totalBenchSize, ioSizes[i]); // ioSize = 64KiB
+			// }
+
+			benchFileChannelWriteThreadPool(fileChannel, totalBenchSize, 4, 64*1024);
+
 			// benchFileChannelWrite(fileChannel, totalBenchSize, 1, 64*1024); // ioSize = 64KiB
 			// benchFileChannelRead(fileChannel, totalBenchSize, 1, 64*1024); // ioSize = 64KiB
 		} catch(IOException ie) {
@@ -52,6 +56,50 @@ public class SSDBench {
 		double iops = totalBenchCount/elapsedTimeS;
 		System.out.println("sequentialWrite,"+thread+","+ioSize+","+bandwidth+","+iops);
 	}
+
+
+	public static void benchFileChannelWriteThreadPool(FileChannel fileChannel, long totalBenchSize, int thread ,int ioSize) throws IOException {
+		assert(totalBenchSize % ioSize == 0);
+		long totalBenchCount = totalBenchSize/ioSize;
+		byte[] data = new byte[ioSize];
+		long curPosition = 0L;
+		long maxPosition = totalBenchSize;
+		ExecutorService executor = Executors.newFixedThreadPool(thread);
+		long startTime = System.nanoTime();    
+		while (curPosition < maxPosition){
+			final long curP = curPosition;
+			executor.execute(()->{
+				try {
+					mySyncWrite(fileChannel, data, curP);
+				} catch(IOException ie) {
+					ie.printStackTrace();
+				}  
+
+			});
+			curPosition += ioSize;
+		}
+		executor.shutdown();
+
+		try {
+		  // Wait a while for existing tasks to terminate
+		  while(!executor.awaitTermination(2, TimeUnit.SECONDS)) {
+			System.out.println("Pool did not terminate, waiting ...");
+		  }
+		} catch (InterruptedException ie) {
+			executor.shutdownNow();
+			ie.printStackTrace();
+		}
+		long elapsedTime = System.nanoTime() - startTime;
+		double elapsedTimeS = (double)elapsedTime/(1000*1000*1000);
+		double totalBenchSizeMiB = totalBenchSize/(1024*1024);
+		double bandwidth =  (totalBenchSizeMiB)/(elapsedTimeS);
+		double iops = totalBenchCount/elapsedTimeS;
+		System.out.println("sequentialWrite,"+thread+","+ioSize+","+bandwidth+","+iops);
+	}
+	public static synchronized void mySyncWrite(FileChannel fileChannel, byte[] data, long curPosition) throws IOException {
+		fileChannel.write(ByteBuffer.wrap(data),curPosition);
+		// fileChannel.force(true);
+	    }
 
 	public static void benchFileChannelRead(FileChannel fileChannel, long totalBenchSize ,int ioSize) throws IOException {
 		int thread = 1;
