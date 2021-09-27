7442fe669e639dde72f458d7cda1cd8ecc6a148f
WYF <w.y.ff@163.com>
2021-09-27 15:49:43 +0800

    delete useless code

--- src/main/java/SSDBench.java
+++ src/main/java/SSDBench.java
@@ -9,23 +9,6 @@ public class SSDBench {
 		try {
 			String dbPath = "/Users/wyf/0code/nsccgz-storage/Cloud-native-programming-contest/testDB/db.data";
 			FileChannel fileChannel = new RandomAccessFile(new File(dbPath), "rw").getChannel();
-			// byte[] data = new byte[4096];
-			// long position = 1024L;
-			// // 指定 position 写入 4kb 的数据
-			// fileChannel.write(ByteBuffer.wrap(data), position);
-			// // 从当前文件指针的位置写入 4kb 的数据
-			// fileChannel.write(ByteBuffer.wrap(data));
-
-			// // 读
-			// ByteBuffer buffer = ByteBuffer.allocate(4096);
-			// position = 1024L;
-			// // 指定 position 读取 4kb 的数据
-			// ret = fileChannel.read(buffer,position);
-			// System.out.println(ret);
-			// // 从当前文件指针的位置读取 4kb 的数据
-			// ret = fileChannel.read(buffer);
-			// System.out.println(ret);
-			// benchFileChannel(fileChannel, 1, 4096);
 			// long totalBenchSize = 1L*1024L*1024L*1024L; // 1GiB
 			long totalBenchSize = 64L*1024L*1024L; // 64MiB
 			benchFileChannelWrite(fileChannel, totalBenchSize, 1, 64*1024); // ioSize = 64KiB
