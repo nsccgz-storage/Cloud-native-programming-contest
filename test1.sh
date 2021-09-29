a2f635b8709f2a8544218b73c66584e1aac5f887
wwyf <w.y.ff@163.com>
2021-09-29 22:19:28 +0800

    update script

---
+++ test1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -rf  /mnt/nvme/mq/*
+
+ls -l /mnt/nvme/mq/
+
+#java  io.openmessaging.Test1 
+#-cp "/home/wyf/nfs/0code/nsccgz-storage/Cloud-native-programming-contest/target/classes:/home/wyf/.m2/repository/org/slf4j/slf4j-log4j12/1.7.6/slf4j-log4j12-1.7.6.jar:/home/wyf/.m2/repository/org/slf4j/slf4j-api/1.7.6/slf4j-api-1.7.6.jar:/home/wyf/.m2/repository/log4j/log4j/1.2.17/log4j-1.2.17.jar:/home/wyf/.m2/repository/com/intel/pmem/llpl/1.2.0-release/llpl-1.2.0
+
+/usr/bin/env /usr/lib/jvm/java-11-openjdk-11.0.12.0.7-0.el7_9.x86_64/bin/java -Dfile.encoding=UTF-8 @/tmp/cp_9zzf273crprs1fknagj5ly7kh.argfile -Xmx32g  -Xss1g io.openmessaging.Test1
+
+ls -l /mnt/nvme/mq/
