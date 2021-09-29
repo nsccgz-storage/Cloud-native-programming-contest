1e5d855330c02d9666cf4c06d7b186dae7306c3b
wwyf <w.y.ff@163.com>
2021-09-29 22:20:47 +0800

    update

--- test1.sh
+++ test1.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+export PATH=/home/wyf/nfs/software/envs/maven/apache-maven-3.8.2/bin:${PATH}
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.275.b01-0.el7_9.x86_64/jre
+
+
 rm -rf  /mnt/nvme/mq/*
 
 ls -l /mnt/nvme/mq/
