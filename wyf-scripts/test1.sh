ca712edcccb13c3ea3a8facce9d4618ed2168c4e
wwyf <w.y.ff@163.com>
2021-10-02 09:31:13 +0800

    update test1

--- wyf-scripts/test1.sh
+++ wyf-scripts/test1.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+
+# init maven settings.xml first
+
+# [wyf@cn3 mqJavaClass]$ cat ~/.m2/settings.xml 
+# <settings>
+#   <proxies>
+#    <proxy>
+#       <id>example-proxy</id>
+#       <active>true</active>
+#       <protocol>http</protocol>
+#       <host>localhost</host>
+#       <port>52341</port>
+#     </proxy>
+#   </proxies>
+# </settings>
+
+set -x
+
 #DBDIR=/mnt/nvme/mq
 DBDIR=/mnt/ssd/mq
 
@@ -8,20 +26,20 @@ ulimit -s unlimited
 export PATH=/home/wyf/nfs/software/envs/maven/apache-maven-3.8.2/bin:${PATH}
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.292.b10-1.el7_9.x86_64/jre
 
+
 mkdir -p ${DBDIR}
+
+# mvn -h
+# exit
+
+mvn clean
+mvn compile
 
 rm -rf  ${DBDIR}/*
 
 ls -l ${DBDIR}
 
-#java  io.openmessaging.Test1 
-#-cp "/home/wyf/nfs/0code/nsccgz-storage/Cloud-native-programming-contest/target/classes:/home/wyf/.m2/repository/org/slf4j/slf4j-log4j12/1.7.6/slf4j-log4j12-1.7.6.jar:/home/wyf/.m2/repository/org/slf4j/slf4j-api/1.7.6/slf4j-api-1.7.6.jar:/home/wyf/.m2/repository/log4j/log4j/1.2.17/log4j-1.2.17.jar:/home/wyf/.m2/repository/com/intel/pmem/llpl/1.2.0-release/llpl-1.2.0
-
-#/usr/bin/env /usr/lib/jvm/java-11-openjdk-11.0.12.0.7-0.el7_9.x86_64/bin/java -Dfile.encoding=UTF-8 @/tmp/cp_9zzf273crprs1fknagj5ly7kh.argfile -Xmx32g  -Xss1g io.openmessaging.Test1
-
-#/usr/bin/env /usr/lib/jvm/java-11-openjdk-11.0.12.0.7-0.el7_9.x86_64/bin/java -Dfile.encoding=UTF-8 @/tmp/cp_9zzf273crprs1fknagj5ly7kh.argfile -Xmx128g  -Xss2g io.openmessaging.TestSSDqueue 
-
 # on cn3
-/usr/bin/env /usr/lib/jvm/java-11/bin/java -Dfile.encoding=UTF-8 @/tmp/cp_9zzf273crprs1fknagj5ly7kh.argfile -Xmx128g  -Xss1g -XX:MaxDirectMemorySize=2g io.openmessaging.Test1  ${DBDIR}
+java -Dfile.encoding=UTF-8 -cp "./target/classes:/home/wyf/nfs/software/envs/mqJavaClass/log4j-1.2.17.jar:/home/wyf/nfs/software/envs/mqJavaClass/llpl-1.2.0-release.jar" -Xmx128g  -Xss1g -XX:MaxDirectMemorySize=2g io.openmessaging.Test1  ${DBDIR}
 
 ls -l ${DBDIR}
