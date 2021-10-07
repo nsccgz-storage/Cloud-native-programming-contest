0d64c0af418168ea0f378d87f2a974e7a00adcf2
wwyf <w.y.ff@163.com>
2021-10-07 20:17:33 +0800

    update

--- src/main/java/io/openmessaging/Test1MessageQueue.java
+++ src/main/java/io/openmessaging/Test1MessageQueue.java
@@ -137,9 +137,9 @@ public class Test1MessageQueue extends MessageQueue {
         // boolean fairLock = true;
         // int writeMethod = 4; 
     
-        int numOfDataFiles = 3;
-        int minBufNum = 8;
-        int minBufLength = 64*1024;
+        int numOfDataFiles = 4;
+        int minBufNum = 6;
+        int minBufLength = 48*1024;
         int timeOutMS = 8;
         boolean fairLock = true;
         int writeMethod = 8; 
