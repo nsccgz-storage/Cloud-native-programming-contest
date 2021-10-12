0970dae576a537b97347af8a819583f5f6a7bacd
Yorkking <2410613187@qq.com>
2021-10-12 22:22:08 +0800

    fix bugs

--- src/main/java/io/openmessaging/Main.java
+++ src/main/java/io/openmessaging/Main.java
@@ -34,7 +34,6 @@ public class Main {
         // Map<Integer, ByteBuffer> res = mq.getRange("12345", 123, 0L, 100);
 
         
-<<<<<<< HEAD
         // String dirPath = "/home/ubuntu/test";
         // String pmemPath = "/pmem";
         // SSDqueue ssdQueue = new SSDqueue(dirPath, pmemPath);
@@ -58,30 +57,6 @@ public class Main {
         //     }
         // }
         
-=======
-        String dirPath = "/mnt/ssd/wyk";
-        String pmemPath = "/mnt/pmem/wyk";
-        SSDqueue ssdQueue = new SSDqueue(dirPath, pmemPath);
-        String t = "1234567890123456789012345678901234567890";
-        System.out.println( t.hashCode());
-        System.out.println(ssdQueue.append("12345", 123, ByteBuffer.wrap(t.getBytes())));
-        System.out.println(ssdQueue.append("12345", 123, ByteBuffer.wrap(t.getBytes())));
-        System.out.println(ssdQueue.append("12345", 123, ByteBuffer.wrap(t.getBytes())));
-        System.out.println(ssdQueue.append("12345", 123, ByteBuffer.wrap(t.getBytes())));
-        System.out.println(ssdQueue.append("12345", 123, ByteBuffer.wrap(t.getBytes())));
-        System.out.println(ssdQueue.append("12345", 123, ByteBuffer.wrap(t.getBytes())));
-        System.out.println(ssdQueue.append("12345", 123, ByteBuffer.wrap(t.getBytes())));
-        System.out.println(ssdQueue.append("dsfsf", 123, ByteBuffer.wrap(t.getBytes())));
-        System.out.println(ssdQueue.append("32424", 256, ByteBuffer.wrap(t.getBytes())));
-
-        for(long idx=0; idx < 10; idx++){
-            Map<Integer, ByteBuffer> res = ssdQueue.getRange("12345", 123, idx, 100);
-            System.out.println("---------****-------------------------");
-            for(Map.Entry<Integer, ByteBuffer> entry: res.entrySet()){
-                System.out.println("" + entry.getKey() + " : " + new String(entry.getValue().array()));
-            }
-        }
->>>>>>> 959e761165bb7a93ec8a606bad7a4ac259014767
     //     ExecutorService executorService = Executors.newCachedThreadPool();
 
     //     Task task = new Task();
@@ -101,7 +76,6 @@ public class Main {
     //         //TODO: handle exception
     //         e.printStackTrace();
     //     }
-<<<<<<< HEAD
         String dbDirPath = "/mnt/ssd/wyk";
         String pmDirPath = "/mnt/pmem/wyk";
         MyLSMessageQueue ssdQueue = new MyLSMessageQueue(dbDirPath, pmDirPath);
@@ -126,8 +100,6 @@ public class Main {
             }
         }
 
-=======
->>>>>>> 959e761165bb7a93ec8a606bad7a4ac259014767
     }
 
 }
