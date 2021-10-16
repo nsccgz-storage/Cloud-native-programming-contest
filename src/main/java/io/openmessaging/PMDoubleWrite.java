51af6e74b4a01fd28d11959058c240a70ffb1e13
WYF <w.y.ff@163.com>
2021-10-16 11:12:41 +0800

    add background double write

---
+++ src/main/java/io/openmessaging/PMDoubleWrite.java
@@ -0,0 +1,252 @@
+package io.openmessaging;
+
+import java.io.IOException;
+import java.nio.channels.AsynchronousFileChannel;
+import java.nio.file.StandardOpenOption;
+import java.nio.channels.CompletionHandler;
+import java.nio.channels.FileChannel;
+import java.nio.ByteBuffer;
+import java.io.RandomAccessFile;
+import java.util.concurrent.Future;
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.LockSupport;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.Function;
+import java.util.function.IntUnaryOperator;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.TreeMap;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+
+import org.apache.log4j.spi.LoggerFactory;
+
+import io.openmessaging.LSMessageQueue.MyByteBufferPool;
+import io.openmessaging.SSDqueue.HotData;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import java.lang.ThreadLocal;
+import java.util.concurrent.Callable;
+import java.lang.management.GarbageCollectorMXBean;
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.lang.management.MemoryUsage;
+import java.lang.Math;
+import java.text.Format;
+import java.util.concurrent.TimeoutException;
+
+import java.util.concurrent.locks.Condition;
+import java.nio.MappedByteBuffer;
+import java.util.Deque;
+
+import com.intel.pmem.llpl.Heap;
+import com.intel.pmem.llpl.MemoryBlock;
+import com.intel.pmem.llpl.MemoryPool;
+
+
+import sun.misc.Unsafe;
+import sun.nio.ch.DirectBuffer;
+import java.lang.reflect.Field;
+
+
+
+import java.util.Comparator;
+
+public class PMDoubleWrite {
+    public static final Logger log = Logger.getLogger(PMDoubleWrite.class);
+	public static final Unsafe UNSAFE;
+	static {
+	    try {
+		Field field = Unsafe.class.getDeclaredField("theUnsafe");
+		field.setAccessible(true);
+		UNSAFE = (Unsafe) field.get(null);
+	    } catch (Exception e) {
+		throw new RuntimeException(e);
+	    }
+	}
+    public static void copyFromDirectBufferToPM(DirectBuffer srcBuf, MemoryPool dstPool, long PMAddr, int length){
+        // TODO: 实现DirectBuffer 复制到 PM （llpl原来不支持这个，不过可以自己实现）
+        // long addr = srcBuf.address();
+        // Field pmAddrField = dstPool.getClass().getDeclaredField("poolAddress");
+        // pmAddrField.setAccessible(true);
+        // long pmPoolAddr = pmAddrField.get(dstPool);
+        // UNSAFE.copyMemory(srcBase, srcOffset, destBase, destOffset, bytes);
+        return ;
+    }
+
+    //TODO: 是否会有cache伪共享问题？，是否要加padding
+    public class ThreadData{
+        ByteBuffer buf[];
+        int curBuf;
+        long curPMAddr; // TODO: 初始化
+        PMBlock block;
+        boolean isFinished;
+        ThreadData(){
+            buf = new ByteBuffer[2];
+            curBuf = 0;
+            buf[0] = ByteBuffer.allocate(1*1024*1024);
+            buf[1] = ByteBuffer.allocate(1*1024*1024);
+            isFinished = false;
+        }
+
+    }
+    ThreadData[]  threadDatas;
+    int maxThreadNum;
+    ExecutorService backgroundDoubleWriteThread;
+    Future<Integer> backgroundDoubleWriteFuture;
+
+    public long totalCapacity;
+    public MemoryPool pool;
+    public PMBlockPool pmBlockPool;
+
+
+
+    PMDoubleWrite(String pmDataFile){
+        log.setLevel(Level.INFO);
+        // log.setLevel(Level.DEBUG);
+
+        totalCapacity = 60L * 1024 * 1024 * 1024;
+        pool = MemoryPool.createPool(pmDataFile, totalCapacity);
+        pmBlockPool = new PMBlockPool(totalCapacity);
+
+        // threadId < 50
+        maxThreadNum = 50;
+        threadDatas = new ThreadData[maxThreadNum];
+        for (int i = 0; i < maxThreadNum; i++){
+            threadDatas[i] = new ThreadData();
+        }
+        backgroundDoubleWriteThread = Executors.newSingleThreadExecutor();
+       
+    }
+
+    public long doubleWrite(int threadId, ByteBuffer data){
+        // 双写，返回PM地址
+        ThreadData td = threadDatas[threadId];
+        if (td.isFinished == true){
+            return -1;
+        }
+        // 判断是否有空位写，如果没有，触发刷盘任务并切换buf
+        if (td.buf[td.curBuf].remaining() < data.remaining()){
+            // 确认刷PM任务完成
+            if (backgroundDoubleWriteFuture != null){
+                while (backgroundDoubleWriteFuture.isDone() != true){
+                    try {
+                        Thread.sleep(1);
+                    } catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+                backgroundDoubleWriteFuture = null;
+            }
+            // 触发刷盘任务，异步调用block的刷盘函数
+            final PMBlock backgroundBlock = td.block;
+            final int flushBuf = td.curBuf;
+            backgroundDoubleWriteFuture = backgroundDoubleWriteThread.submit(new Callable<Integer>(){
+                @Override
+                public Integer call() throws Exception {
+                    pool.copyFromByteArrayNT(td.buf[flushBuf].array(), 0, backgroundBlock.addr , backgroundBlock.capacity);
+                    return 0;
+                }
+            });
+
+            // 申请新block
+            // 如果没法申请了，就停了，不双写了
+            td.block = pmBlockPool.allocate();
+            if (td.block == null){
+                td.isFinished = true;
+                return -1;
+            }
+            // 切换缓冲区
+            td.curBuf = (td.curBuf + 1 ) % 2; 
+        }
+        // 获得当前地址
+        long pmAddr = td.curPMAddr + td.buf[td.curBuf].position();
+        td.buf[td.curBuf].put(data);
+        return pmAddr;
+
+    }
+
+    public class PMBlock {
+        public long addr;
+        public int capacity;
+
+        PMBlock(long a, int c) {
+            addr = a;
+            capacity = c;
+        }
+    }
+
+    public class PMBlockPool {
+        // 分为两个阶段
+        // 阶段1：直接通过移动偏移量的方法申请内存
+        // 依然是分大池子小池子的情况
+        // 定长块管理，单个块的大小
+        public int blockSize;
+        // 线程内可以存放这么多个块
+        public int threadLocalBlockNum;
+        // 总容量
+        public long totalCapacity;
+
+        // 阶段1
+        public int bigBlockSize;
+        // 大池子
+        public AtomicLong atomicGlobalFreeOffset;
+        // 小池子
+        public ThreadLocal<Long> threadLocalBigBlockStartAddr;
+        public ThreadLocal<Integer> threadLocalBigBlockFreeOffset;
+        PMBlockPool(long capacity) {
+            totalCapacity = capacity;
+
+            blockSize = 16*1024*1024; // 16MiB
+            threadLocalBlockNum = 16;
+            bigBlockSize = threadLocalBlockNum * blockSize; // 16*16MiB = 256MiB
+
+            // 初始化阶段1
+            // 大池子
+            atomicGlobalFreeOffset = new AtomicLong();
+            atomicGlobalFreeOffset.set(0L);
+            // 小池子
+            threadLocalBigBlockFreeOffset = new ThreadLocal<>();
+            threadLocalBigBlockStartAddr = new ThreadLocal<>();
+        }
+
+        public PMBlock allocate() {
+            if (threadLocalBigBlockStartAddr.get() == null || threadLocalBigBlockFreeOffset.get() == bigBlockSize) {
+                // 本地没有大块，或者大块满了
+                if (atomicGlobalFreeOffset.get() > totalCapacity) {
+                    // 阶段1可以结束了
+                    return null;
+                }
+                // 分配新的大块
+                long bigBlockStartAddr = atomicGlobalFreeOffset.getAndAdd(bigBlockSize);
+                threadLocalBigBlockStartAddr.set(bigBlockStartAddr);
+                threadLocalBigBlockFreeOffset.set(0);
+            }
+            int freeOffset = threadLocalBigBlockFreeOffset.get();
+            long addr = threadLocalBigBlockStartAddr.get() + freeOffset;
+            threadLocalBigBlockFreeOffset.set(freeOffset + blockSize);
+
+            return new PMBlock(addr, blockSize);
+        }
+
+    }
+}
