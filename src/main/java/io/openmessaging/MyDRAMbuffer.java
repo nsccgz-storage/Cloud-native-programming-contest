e1bb757e1487ed0cfc1cc31d44587fda41f79b88
Yorkking <2410613187@qq.com>
2021-10-21 15:19:56 +0800

    8th try

--- src/main/java/io/openmessaging/MyDRAMbuffer.java
+++ src/main/java/io/openmessaging/MyDRAMbuffer.java
@@ -3,6 +3,7 @@ package io.openmessaging;
 import java.nio.ByteBuffer;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Vector;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
@@ -12,24 +13,52 @@ public class MyDRAMbuffer {
      
     DRAMbuffer buffer0; // small
     DRAMbuffer buffer1; // large
+    DRAMbuffer buffer2; // larger
+    DRAMbuffer buffer3;
+
+    int[] addr2buffer;
+    DRAMbuffer[] bufferArray;
 
     public MyDRAMbuffer(){
-        buffer0 = new DRAMbuffer(1024, 8 * 1024); // 
-        buffer1= new DRAMbuffer(1024, 17 * 1024);
+
+        addr2buffer = new int[]{0, 2048, 4096, 4096 + 1024, 4096 + 1024 + 512};
+        bufferArray = new DRAMbuffer[]{new DRAMbuffer(2048, 2 * 1024), new DRAMbuffer(2048, 4 * 1024), new DRAMbuffer(1024, 8 * 1024), new DRAMbuffer(512 , 17 * 1024)};
     }
     
     public int put(ByteBuffer data){
-        // 负数代表是从 buffer1 分配的，其中从为了使 -1 表示不能分配，所以负值 = - 原本值 - 2
-        if(data.remaining() <= buffer0.slotSize){
-            int addr = buffer0.put(data);
-            if(addr != -1) return addr;
+        // // 负数代表是从 buffer1 分配的，其中从为了使 -1 表示不能分配，所以负值 = - 原本值 - 2
+        // if(data.remaining() <= buffer0.slotSize){
+        //     int addr = buffer0.put(data);
+        //     if(addr != -1) return addr;
+        // }
+        // int addr = buffer1.put(data);
+
+        // if(addr == -1 && buffer2 == null){// 进行一次扩容
+        //     buffer2 = new DRAMbuffer(512, 8 * 1024);
+        // }
+        // return  addr == -1 ? addr : -addr - 2;
+
+        // 地址展开:
+        int dataSize = data.remaining();
+        int addr = -1;
+        for(int i=0; i<4; i++){
+            if(dataSize <= bufferArray[i].capacity){
+                addr = bufferArray[i].put(data);
+                if(addr != -1) return addr + addr2buffer[i];
+            }
         }
-        int addr = buffer1.put(data);
-        return  addr == -1 ? addr : -addr - 2;
+        return addr;
     }
     public ByteBuffer read(int addr, int dataSize){
+        // if(addr == -1) return null;
+        // return addr < 0 ? buffer1.read(-(addr + 2), dataSize) : buffer0.read(addr, dataSize);
         if(addr == -1) return null;
-        return addr < 0 ? buffer1.read(-(addr + 2), dataSize) : buffer0.read(addr, dataSize);
+        for(int i=0 ;i<4; i++){
+            if(addr <= addr2buffer[i+1]){
+                return bufferArray[i].read(addr - addr2buffer[i], dataSize);
+            }
+        }
+        return null;
     }
 
     class DRAMbuffer{
@@ -46,7 +75,7 @@ public class MyDRAMbuffer {
     
             dirBuffer = ByteBuffer.allocateDirect(capacity);
     
-            addrPool = new LinkedList<>();
+            addrPool = new LinkedList<>(); // 考虑自己使用数组实现一个？链表感觉性能不大行
             for(int i=0; i<slotNum; i++){
                 addrPool.offer(i);
             }
@@ -80,7 +109,7 @@ public class MyDRAMbuffer {
             free(addr);
         }
     
-        public ByteBuffer read(int addr, int dataSize){ // 默认 dst 是外面分配好的
+        public ByteBuffer read(int addr, int dataSize){
             int startAddr = addr * slotSize;
            
             ByteBuffer res = dirBuffer.duplicate();
