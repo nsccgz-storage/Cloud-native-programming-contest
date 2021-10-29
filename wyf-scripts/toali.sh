74ea12ae65061097c7c55f64015bef9c1c3381fc
wwyf <w.y.ff@163.com>
2021-10-29 15:21:31 +0800

    update

--- wyf-scripts/toali.sh
+++ wyf-scripts/toali.sh
@@ -2,6 +2,6 @@
 
 # git remote add ali git@code.aliyun.com:shuitang/Cloud-native-programming-contest.git
 
-git pull origin wyf-test1
-git push origin wyf-test1
-git push ali wyf-test1:main -f
+git pull origin wyf-test4
+git push origin wyf-test4
+git push ali wyf-test4:main -f
