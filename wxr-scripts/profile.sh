927ec978dfd627534269f5b4bacee55af25860ce
tanyingkongxin <2352032859@qq.com>
2021-10-31 16:50:56 +0800

    update profile

--- wxr-scripts/profile.sh
+++ wxr-scripts/profile.sh
@@ -1,16 +1,49 @@
-#!/bin/bash
+##!/bin/bash
+#
+#PROFILE_PATH=/home/wyf/nfs/software/envs/mqJavaClass/async-profile/async-profiler-2.5-linux-x64
+#
+#
+#current=`date "+%Y-%m-%d-%H-%M-%S"`
+#
+#LOG_DIR=/home/wxr/project/mylogs/flameGraph/${current}
+##LOG_DIR=./mylogs/test/${current}
+#mkdir ${LOG_DIR}
+#
+#cd ${LOG_DIR}
+#
+#${PROFILE_PATH}/profiler.sh -e cpu -d 60 -f cpu_profile.html MQBench
+#
+##python3 -m http.server
+
+#e!/bin/bash
 
 PROFILE_PATH=/home/wyf/nfs/software/envs/mqJavaClass/async-profile/async-profiler-2.5-linux-x64
 
+set -x
+
 
 current=`date "+%Y-%m-%d-%H-%M-%S"`
-
 LOG_DIR=/home/wxr/project/mylogs/flameGraph/${current}
-#LOG_DIR=./mylogs/test/${current}
 mkdir ${LOG_DIR}
+RESULT_PATH=${LOG_DIR}/
 
-cd ${LOG_DIR}
 
-${PROFILE_PATH}/profiler.sh -e cpu -d 60 -f cpu_profile.html MQBench
+#${PROFILE_PATH}/profiler.sh -d 5 MQBench
+
+cd ${RESULT_PATH}
+
+INTEVEL=10
+ITER=0
+RET=0
+
+while [[ ${RET} -eq 0 ]]
+do
+
+${PROFILE_PATH}/profiler.sh -e cpu -d ${INTEVEL} -f ${ITER}.html MQBench
+RET=$?
+ITER=`expr ${ITER} + ${INTEVEL}`
+
+done
+
 
 #python3 -m http.server
